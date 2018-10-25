f2fdca825251dab0a0a1e2dcb1dc4c9d0385fcd3
Benjamin Lee <ben@2nproductions.com>
2018-10-24 22:26:53 -0700

    Improve debug

--- Release/src/http/client/http_client_asio.cpp
+++ Release/src/http/client/http_client_asio.cpp
@@ -918,7 +918,7 @@ public:
                 auto tcp_port = proxy_type == http_proxy_type::http ? proxy_port : port;
 
                 tcp::resolver::query query(tcp_host, to_string(tcp_port), boost::asio::ip::resolver_query_base::numeric_service);
-                printf("DEBUG calling async_resolve %s %d\n", tcp_host.c_str(), tcp_port);
+                printf("DEBUG calling async_resolve tcp_host %s tcp_port %d\n", tcp_host.c_str(), tcp_port);
                 printf("DEBUG query host name %s service name %s\n" ,query.host_name().c_str(), query.service_name().c_str());
                 auto client = std::static_pointer_cast<asio_client>(ctx->m_http_client);
                 client->m_resolver.async_resolve(query,
