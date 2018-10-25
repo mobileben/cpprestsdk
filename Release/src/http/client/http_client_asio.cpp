71263e2ac0db30c036866c169adb9f87a6f5f4d9
Benjamin Lee <ben@2nproductions.com>
2018-10-25 00:43:30 -0700

    Try and use http as the port

--- Release/src/http/client/http_client_asio.cpp
+++ Release/src/http/client/http_client_asio.cpp
@@ -917,10 +917,10 @@ public:
                 auto tcp_host = proxy_type == http_proxy_type::http ? proxy_host : host;
                 auto tcp_port = proxy_type == http_proxy_type::http ? proxy_port : port;
 
-                tcp::resolver::query query(tcp::v4(), tcp_host, to_string(tcp_port), boost::asio::ip::resolver_query_base::numeric_service);
+                tcp::resolver::query query(tcp_host, "http");
 
-                printf("DEBUG calling async_resolve tcp_host %s tcp_port %d\n", tcp_host.c_str(), tcp_port);
                 printf("DEBUG query host_name %s service_name %s\n" ,query.host_name().c_str(), query.service_name().c_str());
+                printf("DEBUG async_resolve tcp_host %s tcp_port %d\n", tcp_host.c_str(), tcp_port);
 
                 auto client = std::static_pointer_cast<asio_client>(ctx->m_http_client);
                 client->m_resolver.async_resolve(query,
