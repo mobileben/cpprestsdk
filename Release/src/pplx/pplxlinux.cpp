0c8154b22cf55df4abc94180e32aa5f32b69ef03
Benjamin Lee <ben@2nproductions.com>
2019-07-24 23:40:33 -0700

    Use strands

--- Release/src/pplx/pplxlinux.cpp
+++ Release/src/pplx/pplxlinux.cpp
@@ -31,6 +31,8 @@ namespace platform
 	std::vector<std::unique_ptr<boost::asio::detail::thread>> m_threads;
     boost::asio::io_service m_service;
     boost::asio::io_service::work m_work(m_service);
+    boost::asio::io_service::strand strand(m_service);
+
     bool init(false);
 
 _PPLXIMP long GetCurrentThreadId() { return reinterpret_cast<long>(reinterpret_cast<void*>(pthread_self())); }
@@ -64,7 +66,8 @@ _PPLXIMP void linux_scheduler::schedule(TaskProc_t proc, void* param)
 	}
 
     //crossplat::threadpool::shared_instance().service().post(boost::bind(proc, param));
-	    platform::m_service.post(boost::bind(proc, param));
+	    //platform::m_service.post(boost::bind(proc, param));
+	    platform::strand.post(boost::bind(proc, param));
 }
 
 } // namespace details
