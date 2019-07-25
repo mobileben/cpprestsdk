fbd250f4dcde0c096a05a6132c8195be3fb00059
Benjamin Lee <ben@2nproductions.com>
2019-07-24 23:26:30 -0700

    Hack in io_service only for scheduler

--- Release/src/pplx/pplxlinux.cpp
+++ Release/src/pplx/pplxlinux.cpp
@@ -28,6 +28,26 @@ namespace details
 {
 namespace platform
 {
+	std::vector<std::unique_ptr<boost::asio::detail::thread>> m_threads;
+    boost::asio::io_service m_service;
+    boost::asio::io_service::work m_work(m_service);
+    bool init(false);
+
+
+    static void* thread_start(void* arg) CPPREST_NOEXCEPT
+    {
+#if defined(__ANDROID__)
+        // Calling get_jvm_env() here forces the thread to be attached.
+        crossplat::get_jvm_env();
+        pthread_cleanup_push(detach_from_java, nullptr);
+#endif // __ANDROID__
+        platform::m_service.run();
+#if defined(__ANDROID__)
+        pthread_cleanup_pop(true);
+#endif // __ANDROID__
+        return arg;
+    }
+
 _PPLXIMP long GetCurrentThreadId() { return reinterpret_cast<long>(reinterpret_cast<void*>(pthread_self())); }
 
 _PPLXIMP void YieldExecution() { std::this_thread::yield(); }
@@ -35,7 +55,16 @@ _PPLXIMP void YieldExecution() { std::this_thread::yield(); }
 
 _PPLXIMP void linux_scheduler::schedule(TaskProc_t proc, void* param)
 {
-    crossplat::threadpool::shared_instance().service().post(boost::bind(proc, param));
+	if (!platform::init) {
+		platform::m_threads.push_back(
+         std::unique_ptr<boost::asio::detail::thread>(new boost::asio::detail::thread([&] { thread_start(this); })));
+        platform::m_threads.push_back(
+            std::unique_ptr<boost::asio::detail::thread>(new boost::asio::detail::thread([&] { thread_start(this); })));
+        platform::init = true;
+	}
+
+    //crossplat::threadpool::shared_instance().service().post(boost::bind(proc, param));
+	    platform::m_service.post(boost::bind(proc, param));
 }
 
 } // namespace details
